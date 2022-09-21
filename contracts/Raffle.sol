624d9777bb2d761256021ba39cc4d893dbd96bb4
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-09-21 13:20:57 +0000

    Initial commit

---
+++ contracts/Raffle.sol
@@ -0,0 +1,11 @@
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiable random)
+// Winnder selected every X minutes -> completely automated
+// Chainlink Oracle -> Randomness, Autoamted Execution
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Raffle {
+    function enterRaffle() public payable {}
+}
