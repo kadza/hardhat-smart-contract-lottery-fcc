3e997a46baa9c45581bd10adb177a4f99af95155
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-11-01 12:22:03 +0000

    Configured deployment

--- deploy/01-deploy-raffle.ts
+++ deploy/01-deploy-raffle.ts
@@ -1,17 +1,34 @@
+import { ethers, network } from "hardhat"
 import { DeployFunction } from "hardhat-deploy/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { developmentChains, networkConfig } from "../helper-hardhat.config"
 
 const deployRaffle: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployments, getNamedAccounts } = hre
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId ?? 31337
+  const usedNetworkConfig = networkConfig[chainId]
+
+  let vrfCoordinatorV2Address, subscriptionId
+
+  if (developmentChains.includes(network.name)) {
+    const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+    vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address
+    const transactionResponse = await vrfCoordinatorV2Mock.createSubscription()
+    const transactionReceipt = await transactionResponse.wait(1)
+    subscriptionId = transactionReceipt.events[0].args.subId
+  } else {
+    vrfCoordinatorV2Address = usedNetworkConfig.vrfCoordinatorV2
+  }
 
   log("Deploying ...")
 
   await deploy("Raffle", {
     from: deployer,
-    args: [1],
+    args: [vrfCoordinatorV2Address, usedNetworkConfig.raffleEntranceFee, usedNetworkConfig.gasLane],
     log: true,
+    waitConfirmations: 6,
   })
 }
 export default deployRaffle
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,55 @@
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
 import "@nomicfoundation/hardhat-chai-matchers"
-import "hardhat-gas-reporter";
-import "dotenv/config";
-import "solidity-coverage";
-import "hardhat-deploy";
-import "solidity-coverage";
-import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import "solidity-coverage"
+import { HardhatUserConfig } from "hardhat/config"
+
+const MAINNET_RPC_URL =
+  process.env.MAINNET_RPC_URL ||
+  process.env.ALCHEMY_MAINNET_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const POLYGON_MAINNET_RPC_URL =
+  process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+// optional
+const MNEMONIC = process.env.MNEMONIC || "your mnemonic"
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.7",
-    networks: {
-        hardhat: {
-            chainId: 31337,
-        },
-        localhost: {
-            chainId: 31337,
-        },
+  solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
+    localhost: {
+      chainId: 31337,
     },
-    gasReporter: {
-      enabled: false,
+    goerli: {
+      chainId: 5,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY]
+    },
   },
-};
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      5:0,
+    },
+    player: {
+      default: 1,
+      5:1,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+  },
+}
 
-export default config;
+export default config
